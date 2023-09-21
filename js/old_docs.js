da6eabbca2597d801bcec4ffb136005c9b984f10
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-09-22 03:25:32 +0700

    Create old_docs.js

---
+++ js/old_docs.js
@@ -0,0 +1,17 @@
+var options = {
+    text: "Warning! You are reading an older version of this document. To read documentation for the current version, click here.!",
+    duration: 2500,
+    destination: "https://pythainlp.github.io/docs/4.0/",
+    callback: function() {
+    //   console.log("Toast hidden");
+      Toastify.reposition();
+    },
+    close: true,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    }
+  };
+  
+  // Initializing the toast
+  var myToast = Toastify(options);
+  myToast.showToast();

0b12e119297ef60226bb5193984030b82d9c6d03
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2024-05-16 14:17:43 +0700

    Update 5.0 docs

--- 5.0/_static/documentation_options.js
+++ 5.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8b696ac',
+    VERSION: 'e034c54',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
